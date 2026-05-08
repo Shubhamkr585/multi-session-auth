dcfbb71acf7f45dbfafd10c2b75fc46ac33809d9
Shubhamkr585 <shubhamprd585@gmail.com>
2026-05-09 00:06:21 +0530

    config server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import "dotenv/config"
+
+const PORT = process.env.PORT
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
