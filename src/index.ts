29e276680fe1bb76e71335735e9322c20dae3b91
Shubhamkr585 <shubhamprd585@gmail.com>
2026-05-18 19:53:33 +0530

    prisma migration rollback

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ const PORT = process.env.PORT
 
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res:any) => {
     res.send("hello world")
 })
+
+// app.use()
 
 app.listen(3000, () => {
     console.log("server is running on port 3000")
